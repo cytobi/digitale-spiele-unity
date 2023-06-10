ca08b106089e57aad95f1f145803332e5054be62
Tobias Klausner <uowqu@student.kit.edu>
2023-06-10 17:39:06 +0200

    added jump ability

--- digitale-spiele-1/Assets/Scripts/PlayerMovement.cs
+++ digitale-spiele-1/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float turnSpeed = 20f;
     public float movementSpeed = 0.1f;
+    public float jumpSpeed = 2f;
 
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
@@ -22,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float horizontal = Input.GetAxis ("Horizontal");
         float vertical = Input.GetAxis ("Vertical");
-        float jump = Input.GetAxis("Jump");
+        float jumpAxis = Input.GetAxis("Jump");
         
         m_Movement.Set(horizontal, 0f, vertical);
         m_Movement.Normalize ();
@@ -31,11 +32,28 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
 
-        bool wantsToJump = !Mathf.Approximately (jump, 0f);
+        bool wantsToJump = !Mathf.Approximately (jumpAxis, 0f);
+
+        if (wantsToJump && isGrounded) {
+            m_Rigidbody.velocity += new Vector3(0, jumpSpeed, 0);
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation (desiredForward);
         m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * movementSpeed);
         m_Rigidbody.MoveRotation (m_Rotation);
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.name == "Ground") {
+            isGrounded = true;
+            //Debug.Log("grounded");
+        }
+    }
+ 
+    void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.name == "Ground") {
+            isGrounded = false;
+        }
     }
 }
